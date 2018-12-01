5f39a59946b93f9f68e1688ad39dd4c66eba6f4c
Mark Abernathy <mark.abernathy@inin.com>
2018-12-01 13:54:14 -0500

    Make prod build build into github pages

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/main.ts',
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(['dist', 'docs']),
     new HtmlWebpackPlugin({
       title: 'Landing Party'
     })
