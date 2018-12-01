590f0177df54bcdd47be42569444a65f147d4d42
Mark Abernathy <mark.abernathy@inin.com>
2018-12-01 14:48:21 -0500

    Don't clear docs folder on non-prod builds

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/main.ts',
   plugins: [
-    new CleanWebpackPlugin(['dist', 'docs']),
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Landing Party'
     })
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,13 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [
+    new CleanWebpackPlugin(['docs'])
+  ],
   output: {
     path: path.resolve(__dirname, 'docs')
   }
